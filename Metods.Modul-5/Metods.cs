8bf323bb990cdddbf528065d4e65b8a587e281a2
ReLvLGo <Kurt056@mail.ru>
2025-11-08 15:53:15 +0400

    res

--- Metods.Modul-5/Metods.cs
+++ Metods.Modul-5/Metods.cs
@@ -1,39 +1,42 @@
 ﻿using System;
 using System.Net.Mail;
 
-
-
-     int[] GetArrayFromConsole()
+class Mainclass
+{
+    static void Main()
     {
-    
-    var result = new int[5];
-       
-
-        for (int i = 0; i < result.Length; i++)
+        static int[] GetArrayFromConsole()
         {
-            Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-        result[i] =int.Parse(Console.ReadLine());
-    }
-    
-    int temp = 0;
 
-        for (int i = 0; i < result.Length; i++)
+            var result = new int[5];
 
-            for (int j = i + 1; j < result.Length; j++)
-                if (result[i] > result[j])
-                {
-                    temp = result[i];
-                    result[i] = result[j];
-                    result[j] = temp;
-                }
-        for (int i = 0; i < result.Length; i++)
-        {
-            Console.WriteLine(result[i]);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            int temp = 0;
+
+            for (int i = 0; i < result.Length; i++)
+
+                for (int j = i + 1; j < result.Length; j++)
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+
+            return result;
+
+
+
         }
-
-        return result;
-
-
-
     }
-
+}
