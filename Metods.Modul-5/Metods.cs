d526707bee7581175a2cfa1e803037ec7befe857
ReLvLGo <Kurt056@mail.ru>
2025-11-08 22:14:24 +0400

    1

--- Metods.Modul-5/Metods.cs
+++ Metods.Modul-5/Metods.cs
@@ -5,66 +5,62 @@ using System.Drawing;
 class Mainclass
 {
 
+    static void Main(string[] args) { 
+   
+    static string ShowColor()
 
-    static void Main()
-    { }
-        static string ShowColor()
-
-        {
+    {
         Console.WriteLine("Напишите ваши любимые цвета");
-        
+        return Console.ReadLine();
+    }
 
         var favcolor = new string[3];
 
-            for (int i = 0; i < favcolor.Length; i++)
-            {
-                favcolor[i] = ShowColor();
-
-            }
-
-
-            Console.WriteLine("Ваши любимые цвета: ");
-
-
-            foreach (var colors in favcolor)
-            {
-                switch (colors)
-                {
-                    case "red":
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is red!");
-                        break;
-
-                    case "green":
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is green!");
-                        break;
-                    case "cyan":
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is cyan!");
-                        break;
-                    default:
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.ForegroundColor = ConsoleColor.Red;
-
-                        Console.WriteLine("Your color is yellow!");
-                        break;
-                }
-                Console.WriteLine(colors);
-
-            }
-            return ShowColor();
+        for (int i = 0; i < favcolor.Length; i++)
+        {
+            favcolor[i] = ShowColor();
 
         }
 
+
+        Console.WriteLine("Ваши любимые цвета: ");
+
+
+        foreach (var colors in favcolor)
+        {
+            switch (colors)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+            Console.WriteLine(colors);
+
+        }
+        
+
     }
 
-        
-    
-
+}
---
+++ parametrs.Metods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net.Mail;
+
+class Mainclass
+{
+    static void Main(string[] args)
+    {
+         ShowColor();
+        
+    }
+    static string ShowColor(string UserName);
+}
